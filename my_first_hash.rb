02edb98772a2cddb30c1599427c02fdd7326938f
Tymeckia Kendall <tykend@gmail.com>
2019-03-05 04:24:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -42,3 +42,10 @@ def adding
   # add 2 muskets to the shipping_manifest hash below
 
   shipping_manifest["muskets"] = 2
+  
+    shipping_manifest["gun powder"] = 4
+
+  # return the shipping_manifest hash below
+
+  shipping_manifest
+end
